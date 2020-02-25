756eb855990bab2fe76a81399f84386a0bc6e520
maximelef <lefeb.maxime@gmail.com>
2020-02-25 19:30:39 +0100

    Correctif RequeteDownload
    On utilise ConnexionData pour DWD et UPD

--- src/Connexion.java
+++ src/Connexion.java
@@ -90,35 +90,7 @@ public class Connexion extends Thread{
 		// on ferme nous aussi la connexion
 	}
 	
-	public void run () {
-		
-		// On met en place des sockets pour communiquer 
-		// Création d'un socket serveur générique sur le port 40000
-		ServerSocket socketServeur;
-		try {
-			socketServeur = new ServerSocket(this.port);
-			// On attend une connexion puis on l'accepte
-			Socket socket = socketServeur.accept();
-			
-			DataOutputStream entreeSocket = new DataOutputStream(socket.getOutputStream());
-			byte chaine[]; 
-			int retour;
-			FileInputStream file = new FileInputStream("Test/SuperTest");
-			byte[] fileContent = new byte[4096];
-			int offset = 0;
-			while (file.read(fileContent) != -1) {
-				entreeSocket.write(fileContent);
-				System.out.println("--->"+fileContent);
-				//offset =+ fileContent.length;
-			}
-			System.out.println("fini");
-			file.close();
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-					
-	}
+
+	
 	
 }
--- src/RequeteDownload.java
+++ src/RequeteDownload.java
@@ -4,17 +4,16 @@ public class RequeteDownload implements Requete {
 	private String fichier; 
 	private int port;
 	
-	public RequeteDownload(String fichier, int port) {
+	public RequeteDownload(String fichier, String port) {
 		this.fichier = fichier;
-		this.port = port;
+		this.port = Integer.parseInt(port);
 	}
 	
 	@Override
 	public String repondre() {
-		Connexion c = new Connexion (12355);
+		ConnexionData c = new ConnexionData(this.fichier, this.port, "Download/");
 		c.start();
 		return "OK "+this.fichier+" "+this.port;
-		// TODO Auto-generated method stub
 		
 	}
 	
